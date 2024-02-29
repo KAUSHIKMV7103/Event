9ceab82cec97c909cd21308c1ebde679cd34a848
KAUSHIKMV7103 <119048696+KAUSHIKMV7103@users.noreply.github.com>
2024-02-29 14:46:02 +0530

    Add files via upload

---
+++ backend/Day 6/src/main/java/com/event/kaushik/KaushikApplication.java
@@ -0,0 +1,13 @@
+package com.event.kaushik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaushikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaushikApplication.class, args);
+	}
+
+}
